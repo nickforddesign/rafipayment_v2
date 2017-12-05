aad8615f4567d94183ecd036bbd3525c77e36063
Nick Ford <nickforddesign@gmail.com>
2017-12-05 11:03:18 -0500

    finalized build process

--- build/build-staging.js
+++ build/build-staging.js
@@ -9,54 +9,53 @@ const path = require('path')
 const webpack = require('webpack')
 const chalk = require('chalk')
 const config = require('../config')
-const webpackConfig = require('./webpack.staging.conf')
+const webpack_config = require('./webpack.staging.conf')
 const mergeDeepRight = require('ramda/src/mergeDeepRight')
 
-const finalPath = path.resolve(__dirname, '../dist')
+const final_path = path.resolve(__dirname, '../dist')
 
 const spinner = ora('building for staging...')
 spinner.start()
 
-rm(webpackConfig.output.path, err => {
+rm(webpack_config.output.path, err => {
   if (err) throw err
-  webpack(webpackConfig, (err, stats) => {
+  webpack(webpack_config, (err, stats) => {
     spinner.stop()
-    if (err) {
-      console.log('WHOA NOW')
-      throw err
-    } else {
-      rm(finalPath + '/*', err => {
-        if (err) throw err
 
-        move(webpackConfig.output.path + '/*', finalPath)
-          .then(() => {
-            rm(webpackConfig.output.path, err => {
-              if (err) throw err
+    // check for errors in build and in project
+    if (err || stats.hasErrors()) throw err
 
-              process.stdout.write(stats.toString({
-                colors: true,
-                modules: false,
-                children: false,
-                chunks: false,
-                chunkModules: false
-              }) + '\n\n')
+    rm(final_path + '/*', err => {
+      if (err) throw err
 
-              if (stats.hasErrors()) {
-                console.log(chalk.red('  Build failed with errors.\n'))
-                process.exit(1)
-              }
+      move(webpack_config.output.path + '/*', final_path)
+        .then(() => {
+          rm(webpack_config.output.path, err => {
+            if (err) throw err
 
-              console.log(chalk.cyan('  Build complete.\n'))
-              console.log(chalk.yellow(
-                '  Tip: built files are meant to be served over an HTTP server.\n' +
-                '  Opening index.html over file:// won\'t work.\n'
-              ))
-            })
+            process.stdout.write(stats.toString({
+              colors: true,
+              modules: false,
+              children: false,
+              chunks: false,
+              chunkModules: false
+            }) + '\n\n')
+
+            if (stats.hasErrors()) {
+              console.log(chalk.red('  Build failed with errors.\n'))
+              process.exit(1)
+            }
+
+            console.log(chalk.cyan('  Build complete.\n'))
+            console.log(chalk.yellow(
+              '  Tip: built files are meant to be served over an HTTP server.\n' +
+              '  Opening index.html over file:// won\'t work.\n'
+            ))
           })
-          .catch(err => {
-            throw err
-          })
-      })
-    }
+        })
+        .catch(err => {
+          throw err
+        })
+    })
   })
 })
--- build/build.js
+++ build/build.js
@@ -4,69 +4,58 @@ process.env.NODE_ENV = 'production'
 
 const ora = require('ora')
 const rm = require('rimraf')
-const mv = require('mv')
+const move = require('glob-move')
 const path = require('path')
 const webpack = require('webpack')
 const chalk = require('chalk')
 const config = require('../config')
-const webpackConfig = require('./webpack.build.conf')
+const webpack_config = require('./webpack.production.conf')
 const mergeDeepRight = require('ramda/src/mergeDeepRight')
 
-const finalPath = path.resolve(__dirname, '../dist')
+const final_path = path.resolve(__dirname, '../dist')
 
 const spinner = ora('building for production...')
 spinner.start()
 
-rm(webpackConfig.output.path, err => {
+rm(webpack_config.output.path, err => {
   if (err) throw err
-  webpack(webpackConfig, (err, stats) => {
+  webpack(webpack_config, (err, stats) => {
     spinner.stop()
-    if (err) throw err
 
-    process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n')
+    // check for errors in build and in project
+    if (err || stats.hasErrors()) throw err
 
-    if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
-      process.exit(1)
-    }
-
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
-    
-    rm(finalPath, err => {
+    rm(final_path + '/*', err => {
       if (err) throw err
 
-      mv(webpackConfig.output.path, finalPath, err => {
-        if (err) throw err
+      move(webpack_config.output.path + '/*', final_path)
+        .then(() => {
+          rm(webpack_config.output.path, err => {
+            if (err) throw err
 
-        process.stdout.write(stats.toString({
-          colors: true,
-          modules: false,
-          children: false,
-          chunks: false,
-          chunkModules: false
-        }) + '\n\n')
+            process.stdout.write(stats.toString({
+              colors: true,
+              modules: false,
+              children: false,
+              chunks: false,
+              chunkModules: false
+            }) + '\n\n')
 
-        if (stats.hasErrors()) {
-          console.log(chalk.red('  Build failed with errors.\n'))
-          process.exit(1)
-        }
+            if (stats.hasErrors()) {
+              console.log(chalk.red('  Build failed with errors.\n'))
+              process.exit(1)
+            }
 
-        console.log(chalk.cyan('  Build complete.\n'))
-        console.log(chalk.yellow(
-          '  Tip: built files are meant to be served over an HTTP server.\n' +
-          '  Opening index.html over file:// won\'t work.\n'
-        ))
-      })
+            console.log(chalk.cyan('  Build complete.\n'))
+            console.log(chalk.yellow(
+              '  Tip: built files are meant to be served over an HTTP server.\n' +
+              '  Opening index.html over file:// won\'t work.\n'
+            ))
+          })
+        })
+        .catch(err => {
+          throw err
+        })
     })
   })
 })
